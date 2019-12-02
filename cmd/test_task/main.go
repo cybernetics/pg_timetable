b6f19d173743a3c2dd69b8b1a6bcca9fc2257daf
Pavlo Golub <pavlo.golub@gmail.com>
2019-12-02 23:56:37 +0100

    - use go-flags

--- cmd/test_task/main.go
+++ cmd/test_task/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+
 	scheduler "github.com/cybertec-postgresql/pg_timetable/internal/scheduler"
+	flags "github.com/jessevdk/go-flags"
 )
 
 /**
  * test_task is the utility to test shell tasks before using them in pg_timetable chains
  */
 
+type cmdOptions struct {
+	TaskName string   `short:"t" long:"taskname" description:"The name of the built-in task or an executable" required:"True"`
+	Argument []string `short:"a" long:"argument" description:"Arguments to call in JSON notation"`
+}
+
+var opts cmdOptions
+
 func main() {
-	cmdPtr := flag.String("cmd", `bash`, "command to run")
-	cmdArgs := flag.String("arg", `["-c", "uconv -x \"::Latin; ::Latin-ASCII;\" > orte_ansi.txt < orte.txt"]`, "arguments for the command")
-	exitCode, err := scheduler.ExecuteShellCommand(*cmdPtr, []string{*cmdArgs})
+	_, err := flags.Parse(&opts)
+	fmt.Println(opts.Argument)
+	exitCode, err := scheduler.ExecuteShellCommand(opts.TaskName, opts.Argument)
 	if err != nil {
 		fmt.Println(err)
 	}
